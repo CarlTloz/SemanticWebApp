3c3c685659c4a0364e3242e2ecc880dd7f593d29
Jhosef Cardich <jhosef.cardich@gmail.com>
2019-10-30 18:09:59 +0100

    Metodos para la gestion del html

--- src/main/java/main/java/ExternalQueryMain.java
+++ src/main/java/main/java/ExternalQueryMain.java
@@ -10,10 +10,18 @@ public class ExternalQueryMain {
         return rs;
     }
 
+
+
     public static void printResult(HashMap<String, HashMap> rs , int size) {
+
+        //Antes de la  consulta creamos el header para poder poner archivo
+        generateHtmlHeader ();
+
         for (String variable : (ArrayList<String>) rs.get("result").get("variables")) {
+
             System.out.print(String.format("%-"+size+"."+size+"s", variable ) + " | ");
         }
+        String html = "<HTML>"+rs.+"</HTML>"
         System.out.print("\n");
         for (HashMap value : (ArrayList<HashMap>) rs.get("result").get("rows")) {
             for (String variable : (ArrayList<String>) rs.get("result").get("variables")) {
@@ -49,6 +57,70 @@ public class ExternalQueryMain {
         }
 
     }
+
+    //+++++++++++++++++++++++++++++++++++ METODOS PARA GESTIONAR EL HTML +++++++++++++++++++++++++++++++
+
+    //+++++++++++++++++++++++++++++++++++  CREACION DEL ARCHIVO +++++++++++++++++++++++++++++++
+    public static void createHtmlFile(){
+        String cabezera_html = "<html>\n" +
+                "\n" +
+                "<head>\n" +
+                "<title>El primer documento HTML</title>\n" +
+                "</head>\n" +
+                "\n" +
+                "<body>";
+    }
+
+    public static void createHtmlStream(){
+        String cabezera_html = "<html>\n" +
+                "\n" +
+                "<head>\n" +
+                "<title>El primer documento HTML</title>\n" +
+                "</head>\n" +
+                "\n" +
+                "<body>";
+    }
+
+    //+++++++++++++++++++++++++++++++++++ APERTURA DEL ARCHIVO +++++++++++++++++++++++++++++++
+
+    public static void openHtmlStream(){
+        String cabezera_html = "<html>\n" +
+                "\n" +
+                "<head>\n" +
+                "<title>El primer documento HTML</title>\n" +
+                "</head>\n" +
+                "\n" +
+                "<body>";
+    }
+
+
+
+    //+++++++++++++++++++++++++++++++++++ GENERACION DE PARTES DEL ARCHIVO+++++++++++++++++++++++++++++++
+
+    public static void generateHtmlHeader(){
+        String cabezera_html = "<html>\n" +
+                "\n" +
+                "<head>\n" +
+                "<title>El primer documento HTML</title>\n" +
+                "</head>\n" +
+                "\n" +
+                "<body>";
+    }
+
+
+
+    public static void generateHtmlFooter(){
+        String cabezera_html = "<html>\n" +
+                "\n" +
+                "<head>\n" +
+                "<title>El primer documento HTML</title>\n" +
+                "</head>\n" +
+                "\n" +
+                "<body>";
+    }
+
+
+    //+++++++++++++++++++++++++++++++++++ ESCRITURA SOBRE EL ARCHIVO ++++++++++++++++++++++++++++++
 
     public static void main(String[] args) {
         externalSparklQuery();
