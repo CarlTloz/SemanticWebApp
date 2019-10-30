b0825b7b6c11870a7b4481fc4ae5393075513d34
Jhosef Cardich <jhosef.cardich@gmail.com>
2019-10-30 18:37:57 +0100

    añadidos mas metodos para la gestion del html

--- src/main/java/main/java/ExternalQueryMain.java
+++ src/main/java/main/java/ExternalQueryMain.java
@@ -1,8 +1,15 @@
 package main.java;// https://github.com/BorderCloud/SPARQL-JAVA
+import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class ExternalQueryMain {
+
+    //Elementos para la gestion de archivos
+    private FileReader aProcesar;
+    private	FileWriter salidaTokens;
+    private PrintWriter printLineTokens;
+
 
     public static HashMap<String, HashMap> retrieveData(String endpointUrl, String query) throws EndpointException {
         Endpoint sp = new Endpoint(endpointUrl, false);
@@ -61,41 +68,68 @@ public class ExternalQueryMain {
     //+++++++++++++++++++++++++++++++++++ METODOS PARA GESTIONAR EL HTML +++++++++++++++++++++++++++++++
 
     //+++++++++++++++++++++++++++++++++++  CREACION DEL ARCHIVO +++++++++++++++++++++++++++++++
-    public static void createHtmlFile(){
-        String cabezera_html = "<html>\n" +
-                "\n" +
-                "<head>\n" +
-                "<title>El primer documento HTML</title>\n" +
-                "</head>\n" +
-                "\n" +
-                "<body>";
+
+    public static FileWriter createHtmlFile() throws IOException {
+        String rutaSalida = "/OutPut.txt";
+        FileWriter queryOutputFile = new FileWriter(rutaSalida);
+        return queryOutputFile ;
     }
 
-    public static void createHtmlStream(){
-        String cabezera_html = "<html>\n" +
-                "\n" +
-                "<head>\n" +
-                "<title>El primer documento HTML</title>\n" +
-                "</head>\n" +
-                "\n" +
-                "<body>";
+    public static PrintWriter createHtmlStream(FileWriter a ){
+        PrintWriter outFilePrinter = new   PrintWriter(a);
+        return outFilePrinter;
     }
+
+    public void registerToHtml (String token) {
+        this.printLineTokens.println(token);
+    }
+
 
     //+++++++++++++++++++++++++++++++++++ APERTURA DEL ARCHIVO +++++++++++++++++++++++++++++++
 
-    public static void openHtmlStream(){
-        String cabezera_html = "<html>\n" +
-                "\n" +
-                "<head>\n" +
-                "<title>El primer documento HTML</title>\n" +
-                "</head>\n" +
-                "\n" +
-                "<body>";
-    }
+    public  FileReader openHtmlFile (String ruta) {
+
+        FileReader fichero = null;
+        try {
+            //Abrir el fichero indicado en la variable nombreFichero
+            fichero = new FileReader(ruta);
+            //Leer el primer carácter
+            //Se debe almacenar en una variable de tipo int
+
+            return fichero;
+            //leeFichero(fichero);
 
 
+        }
+        catch (FileNotFoundException e) {
+            //Operaciones en caso de no encontrar el fichero
+            System.out.println("Error: Fichero no encontrado");
+            //Mostrar el error producido por la excepción
+            System.out.println(e.getMessage());
+        }
+        catch (Exception e) {
+            //Operaciones en caso de error general
+            System.out.println("Error de lectura del fichero");
+            System.out.println(e.getMessage());
+        }
+        finally {
+            //Operaciones que se harán en cualquier caso. Si hay error o no.
+            try {
+                //Cerrar el fichero si se ha abierto
+                //if(fichero != null)
+                //	fichero.close();
+            }
+            catch (Exception e) {
+                System.out.println("Error al cerrar el fichero");
+                System.out.println(e.getMessage());
+            }
+        }
 
-    //+++++++++++++++++++++++++++++++++++ GENERACION DE PARTES DEL ARCHIVO+++++++++++++++++++++++++++++++
+        return fichero;
+        }
+
+
+        //+++++++++++++++++++++++++++++++++++ GENERACION DE PARTES DEL ARCHIVO+++++++++++++++++++++++++++++++
 
     public static void generateHtmlHeader(){
         String cabezera_html = "<html>\n" +
